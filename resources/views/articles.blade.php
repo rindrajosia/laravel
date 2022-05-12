d40037da0d769de2b4f5978a152b5077fb17705d
rindrajosia <rrindrajosia@gmail.com>
2022-05-12 20:14:49 +0300

    Update target link href list articles

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -5,7 +5,7 @@
 
     @if($posts->count() > 0)
       @foreach($posts as $post)
-        <h2><a href="#">{{ $post->title }}</a></h2>
+        <h2><a href="{{ route('post.show', ["id" => $post->id])}}">{{ $post->title }}</a></h2>
       @endforeach
     @else
       <span>Pas d'article</span>
