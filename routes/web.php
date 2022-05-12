0b62124755341e5ec103b22897ccbb93960f4010
rindrajosia <rrindrajosia@gmail.com>
2022-05-12 20:11:00 +0300

    Update show page route name

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('welcome');
 
-Route::get('/post/{id}', [PostController::class, 'show'])->WhereNumber('id');
+Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
 
 Route::get('/contact', [PostController::class, 'contact'])->name('contact');
