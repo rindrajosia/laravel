970e72eb7b8817e11ee1e42b3757357a11eaff35
rindrajosia <rrindrajosia@gmail.com>
2022-05-16 20:28:33 +0300

    Add relation to our models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Comment;
+use App\Models\Image;
 
 class Post extends Model
 {
@@ -15,5 +16,10 @@ class Post extends Model
     public function comments()
     {
       return $this->hasMany(Comment::class);
+    }
+
+    public function image()
+    {
+      return $this->hasOne(Image::class);
     }
 }
